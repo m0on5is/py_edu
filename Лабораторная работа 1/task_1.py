da5adde6ef9b5ec9eda0b2a04b1319b204c4b068
m0on5is <novozhilov.egor10@gmail.com>
2024-10-24 20:31:46 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,16 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# TODO заменить значение пропущенного элемента средним арифметическим
-sum_ = 0
-for i in range(0, 20):
-    if numbers[i] != None:
-        sum_ += numbers[i]
+index_none = 4
 
+sum_ = sum(numbers[:index_none]) + sum(numbers[index_none+1:])
 len_ = len(numbers)
-average = sum_ / len_
+average_of_numbers = sum_ / len_
 
-for i in range(0, 20):
-    if numbers[i] == None:
-        numbers[i] = average
-
+numbers[index_none] = average_of_numbers
 print("Измененный список:", numbers)
